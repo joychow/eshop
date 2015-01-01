e57f55d8ba3db8f75950fde746d8eaa2b28108fe
joy chow <zhouzh@gmail.com>
2015-01-01 15:03:38 +0800

    add category feature

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+  belongs_to :category
+  
+  validates :name, presence: true
+  validates :price, presence: true, numericality: {greater_than: 0}
+  validates :category_id, presence: true
 end
