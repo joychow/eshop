2dfa6b68a047ed8799ced781d9ccfecfd39804f4
joy chow <zhouzh@gmail.com>
2015-01-26 00:11:14 +0800

    add admin auth!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :administrators
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :partners
   resources :products, only: [:show, :index]
 
